43e00a9869caf1007006deb083479dfd36777615
Victor Sydorov <Victor.sydorov@gmail.com>
2023-07-09 20:13:05 +0300

    UC#14 Infrastructure setup

---
+++ Use_Case_14/Use_Case_14/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Use_Case_14.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
